97605c0cd5edce623354f579ed5a1d27452c12ad
Ze1DoS <113006937+Ze1DoS@users.noreply.github.com>
2023-11-10 21:14:17 +0300

    Update cat2.c

--- cat2.c
+++ cat2.c
@@ -7,31 +7,67 @@ int main(int argc, char *argv[]) {
     int number = 1;
     fileName = fopen(argv[argc-1], "r");
     if (strcmp(argv[argc-2], "-n") == 0) {
-        while(fgets(line, 100, fileName) != NULL){
+        while(fgets(line, sizeof(line), fileName) != NULL){
             printf("%d %s", number, line);
             number++;
         }
     } else if(strcmp(argv[argc-2], "-b") == 0) {
-        while(fgets(line, 100, fileName) != NULL){
-            int prevLine = '\n';
-            if (line[0] != '\n' || prevLine != '\n'){
-                printf("%d %s", number, line);
-                number ++;
+        while(fgets(line, sizeof(line), fileName) != NULL){
+            if (line[0] != '\n'){
+                printf("%d ", number);
+                number++;
             }
+            printf("%s", line);
         }
     } else if (strcmp(argv[argc-2], "-s") == 0) {
-        int emptyLine = 0;
-        while(fgets(line, 100, fileName) != NULL){
-            if (line[0] != '\n') {
+        int prevLine = '\n';
+        while(fgets(line, sizeof(line), fileName) != NULL) {
+            if (line[0] != '\n' || prevLine != '\n') {
                 printf("%s", line);
-
-                emptyLine = 0;
-            } else if (!emptyLine) {
-                printf("%s", line);
-
-                emptyLine = 1;
+            }
+            prevLine =line[0];
+        }
+    } else if(strcmp(argv[argc-2], "-e") == 0) {
+        while (fgets(line, sizeof(line), fileName) != NULL) {
+            int len = strlen(line);
+            if (line[len - 1] == '\n') {
+                line[len - 1] = '$';
+                printf("%s\n", line);
+            } else {
+                printf("%s$", line);
             }
         }
-    }
+    } else if (strcmp(argv[argc - 2], "-t") == 0) {
+        while (fgets(line, sizeof(line), fileName) != NULL) {
+            for (int i = 0; i < strlen(line); i++) {
+                if (line[i] == '\t') {
+                    printf("^I");
+                } else {
+                    printf("%c", line[i]);
+                }
+            }
+        }
+    } else if(strcmp(argv[argc-2], "--number-nonblank") == 0) {
+        while(fgets(line, sizeof(line), fileName) != NULL){
+            if (line[0] != '\n'){
+                printf("%d ", number);
+                number++;
+            }
+            printf("%s", line);
+        }
+    } else if (strcmp(argv[argc-2], "--number") == 0) {
+        while(fgets(line, sizeof(line), fileName) != NULL){
+            printf("%d %s", number, line);
+            number++;
+        }
+    } else if (strcmp(argv[argc-2], "--squeeze-blank") == 0) {
+        int prevLine = '\n';
+        while(fgets(line, sizeof(line), fileName) != NULL) {
+            if (line[0] != '\n' || prevLine != '\n') {
+                printf("%s", line);
+            }
+            prevLine =line[0];
+        }
+    } 
     fclose(fileName);
 }
